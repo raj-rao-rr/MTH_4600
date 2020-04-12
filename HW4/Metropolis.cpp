741454b2a43d3a52f32148fae0a946c5266194b3
Rajesh R <34293842+Raj9898@users.noreply.github.com>
2020-04-12 12:14:41 -0400

    Update Files

--- HW4/Metropolis.cpp
+++ HW4/Metropolis.cpp
@@ -38,7 +38,10 @@ int main () {
    /////////////////////////////////////////////////////////////////////////////
    // Your Metropolis algorithm starts here...
 
-   //// problem 1. 
+   //////////////////////////////////////////////////////////////////
+   // problem 1. 
+   //////////////////////////////////////////////////////////////////
+
    //// variable initialization 
    //double **I, **actual_mv;
    //double **e = Array(1, 50);
@@ -131,7 +134,7 @@ int main () {
    //    }
    //}
 
-   //// Output the invariant distribution 
+   //// Report the best-found portfolio and its variance here.
    //printf("The calculated minimum variance portoflio is\n");
    //for (int i = 1; i <= 50; ++i) {
    //    printf("%8.4f\n", EX[i][1]);
@@ -141,15 +144,109 @@ int main () {
    //// compute the error of the projection
    //error = MSE(actual_mv, EX, 50.0);
    //printf("Our mean squared error is % 8.8f\n", error);
+   
 
+
+   //////////////////////////////////////////////////////////////////
    // problem 2.
+   //////////////////////////////////////////////////////////////////
+
+   //// variable initialization 
+   //double rho, c, U, var1, var2, error, deltaVar, T = 0.000001;
+   //int i1, i2, flag, seed=0;
+
+   //// generates an initial vector for our invariant distribution 
+   //double** E0 = Array(50, 1); 
+   //for (int i = 1; i <= 50; ++i) {
+   //    E0[i][1] = 0.0200;
+   //}
+
+   //double** EX = Array(50, 1);
+   //for (int i = 1; i <= 50; ++i) {
+   //    EX[i][1] = E0[i][1];
+   //}
+
+   //// calculate the portfolio variance for original weight
+   //var1 = Variance(E0, V);
+
+   //printf("Simulation begins...\n");
+   //// MONTE CARLO SIMULATION
+   //for (int j = 1; j <= 2000000; ++j) {
+
+   //    // selects our stock weights to swap 
+   //    while (1) {
+   //        // Pick two numbers independently and uniformly from {1,...,50}.
+   //        i1 = 1 + int(MTUniform(seed) * 50); // index of element to reduce weight
+   //        i2 = 1 + int(MTUniform(seed) * 50); // index of element to add weight
+  
+   //        // See if they are acceptable, i.e, if they satisfy (1) and (2) above.
+   //        if (i1 != i2) {
+   //            break;
+   //        }
+   //    }
+   //   
+   //    // building the neighbor to the state
+   //    EX[i1][1] -= 0.0001;
+   //    EX[i2][1] += 0.0001;
+
+   //    // Check to see if there is a short position in the weights
+   //    flag = sFlag(EX, 50);
+
+   //    if (flag == 0) {
+   //        // calculate the portfolio variance for weight 2
+   //        var2 = Variance(EX, V);
+   //    }
+   //    else {
+   //        var2 = 1000;
+   //    }
+
+   //    // compute the change in variances
+   //    deltaVar = var2 - var1;
+
+   //    // if neighbor state is lower than prior, use the new state as our weight
+   //    if (deltaVar <= 0) {
+   //        var1 += deltaVar;
+   //    }
+   //    // if neighbor is larger than prior, use accept/reject scheme 
+   //    else {
+   //        U = MTUniform(seed); // generate a fresh uniform  
+   //        rho = exp(-deltaVar / T);
+
+   //        // if U < our rho we modify our weight to the new neighbor 
+   //        if (U <= rho) {
+   //            var1 += deltaVar;
+   //        }
+   //        else {
+   //            // returning back to the previous state 
+   //            EX[i1][1] += 0.0001;
+   //            EX[i2][1] -= 0.0001;
+   //        }
+   //    }
+
+   //    if (j % 100000 == 0) {
+   //        printf("At sim %8.4d -> variance is %8.8f\n", j, var1);
+   //    }
+   //}
+
+   //// Report the best-found portfolio and its variance here.
+   //printf("The calculated minimum variance portoflio is\n");
+   //for (int i = 1; i <= 50; ++i) {
+   //    printf("%8.4f\n", EX[i][1]);
+   //}
+   //printf("The minimum variance reached is %8.8f\n", var1);
+
+
+
+   //////////////////////////////////////////////////////////////////
+   // problem 3.
+   //////////////////////////////////////////////////////////////////
 
    // variable initialization 
-   double rho, c, U, var1, var2, error, deltaVar, T = 0.000001;
-   int i1, i2, flag, seed=0;
+   double rho, c, U, var1, var2, error, deltaVar, T = 0.02;
+   int i1, i2, flag, seed = 0;
 
    // generates an initial vector for our invariant distribution 
-   double** E0 = Array(50, 1); 
+   double** E0 = Array(50, 1);
    for (int i = 1; i <= 50; ++i) {
        E0[i][1] = 0.0200;
    }
@@ -171,13 +268,13 @@ int main () {
            // Pick two numbers independently and uniformly from {1,...,50}.
            i1 = 1 + int(MTUniform(seed) * 50); // index of element to reduce weight
            i2 = 1 + int(MTUniform(seed) * 50); // index of element to add weight
-  
+
            // See if they are acceptable, i.e, if they satisfy (1) and (2) above.
            if (i1 != i2) {
                break;
            }
        }
-      
+
        // building the neighbor to the state
        EX[i1][1] -= 0.0001;
        EX[i2][1] += 0.0001;
@@ -221,24 +318,23 @@ int main () {
        }
    }
 
-   // Output the invariant distribution 
+   // Report the best-found portfolio and its variance here.
    printf("The calculated minimum variance portoflio is\n");
    for (int i = 1; i <= 50; ++i) {
        printf("%8.4f\n", EX[i][1]);
    }
    printf("The minimum variance reached is %8.8f\n", var1);
 
-   // problem 3.
 
-
+   //////////////////////////////////////////////////////////////////
    // problem 4.
+   //////////////////////////////////////////////////////////////////
 
 
 
    // and ends here. 
    /////////////////////////////////////////////////////////////////////////////
 
-   // Report the best-found portfolio and its variance here.
 
    // Pause so the execution window does not close.
    Exit ();
