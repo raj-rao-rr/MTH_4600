236ad3d0bb1d96bd253400df73b7d45371d7e93b
Rajesh R <34293842+Raj9898@users.noreply.github.com>
2020-04-27 13:03:50 -0400

    Update Files

--- HW5/StoVol.cpp
+++ HW5/StoVol.cpp
@@ -30,7 +30,7 @@ int main () {
    avg1 = 0.0;
 
    // number of simulations to run
-   sims = 1000000;
+   sims = 1;
 
    // Time to expiration.
    T = 0.5;
@@ -98,7 +98,7 @@ int main () {
       // Problem 2 //
       for (j = 0; j < 11; ++j) {
           // itterate through each strike value and subtract from terminal strike price  
-          // discount each terminal cash flow to the final value
+          printf("%8.4f,%8.4f\n ", s, strikes[j]);
           val2 = exp(-0.05 * 0.5) * (s - strikes[j]);
           averages[j] += Max(val2);
       }
@@ -111,7 +111,7 @@ int main () {
 
    // Outputs value for problem 1
    printf("\nProblem 1:\n");
-   printf("Our call option with strike K = 0 is valued at %8.4f\n",  avg1 / sims);
+   printf("Our call option with strike K = 0 is valued at %8.4f\n", (avg1 / sims));
    printf("--------------------------------------------------------------\n");
 
    // Outputs value for problem 2
@@ -119,7 +119,7 @@ int main () {
    printf("Problem 2:\n");
    for (int t = 0; t < 11; ++t) {
        K = strikes[t];
-       printf("Our call option with strike K = %8.2f is valued at %8.4f\n", K, averages[t] / sims);
+       printf("Our call option with strike K = %8.2f is valued at %8.4f\n", K, (averages[t] / sims));
    }
    printf("--------------------------------------------------------------\n");
 
@@ -128,11 +128,11 @@ int main () {
    // define x, y arrays
    printf("Problem 3:\n");
    for (int t = 0; t < 11; ++t) {
-       val3 = averages[t] / sims;
+       val3 = (averages[t] / sims);
        K = strikes[t];
        vol = 100.0*ImpliedVol(0.5, 100, K, 0.05, val3);
 
-       printf("Our call option with strike K = %8.2f has implied volatility at %8.4f\n", K, vol);
+       printf("Our call option with strike K = %8.2f has implied volatility at %8.4f%\n", K, vol);
    }
    printf("--------------------------------------------------------------\n");
 
