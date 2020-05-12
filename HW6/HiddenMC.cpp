91ab7b122d2779917364ea1d52a7fe3fa3c7796a
Rajesh R <34293842+Raj9898@users.noreply.github.com>
2020-05-11 22:27:11 -0400

    Update Files

--- HW6/HiddenMC.cpp
+++ HW6/HiddenMC.cpp
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Starter code for Homework 6.
 ////////////////////////////////////////////////////////////////////////////////
-
+#define _USE_MATH_DEFINES
 
 // These functions are found below.
 void GetData ();
@@ -11,53 +11,94 @@ void Report ();
 double *R, *R2, *sigma;
 
 #include "Functions.h"
+#include <iostream>
+#include <cmath>
+
 
 int main () {
 
-   double *s, *p, *ptilde, *g;
-   double sigma0, alpha;
+   double *s, *p, *ptilde, *g, *annual_vol;
+   double sigma0, alpha, sqrt2pi, k_hat_max, Z, max_probT, current_probT;
+   const double pi = M_PI;
    double *AllocateMemory();
-   int allocation;
+   int index, t, k, i;
 
    // Allocate necessary memory.
    s = AllocateMemory ();
    p = AllocateMemory ();
    ptilde = AllocateMemory ();
    g = AllocateMemory ();
-
+   annual_vol = (double*)calloc(1260, sizeof(double));
 
    // Read in the time series data.
    GetData ();
-
-   // Implement the hidden Markov chain model between here...
 
    // assigning the free parameters 
    sigma0 = 0.25;
    alpha = 0.03;
 
-   for (int i = -1258; i <= 1258; ++i) {
-    
-       // the daily volatility when the walk is in state k (only positive probability are in steps of 2) 
-       s[1259 + i] = sigma0 * exp(alpha * i);
+   // compute the stationary value of sqrt(2*pi)
+   sqrt2pi = sqrt(2 * pi);
 
-       //// constructing the conditional volatility based on D
-       //if (-1257 <= i <= 1257) {
-       //    p[1259 + i] = (i);
-       //}
+   p[1258] = 1; // P_D_0 [E] = P [E | Omega] = P [E]
+
+   // incrementing the time process daily 
+   for (t = 1; t <= 1258; ++t) {
+        
+       // Initialize certain variables
+       k_hat_max = 0.0;   // k value that maximizes the conditional probability 
+       max_probT = 0.0;   // maximum value for the conditional probability at time t  
+       Z = 0.0;           // sum of terms up to g_k
+
+       // cyclig through states k with postive probability
+       for (k = -t; k <= t; k += 2) {
+
+           // index for all the positions of k - including both postive and negative states
+           index = k + 1258;
+
+           //////////////////////////////
+           // 1. Volatility Estimation 
+           //////////////////////////////
+
+           // estimating the volatility at time W_t from D_{t-1} -> P_D_{t-1} [W_t = k] 
+           current_probT = 0.5 * (p[index + 1] + p[index - 1]);
+
+           // determine the argument value k that maximizes the probability P_D_{t-1} [W_t = k] 
+           if (current_probT > max_probT) {
+               k_hat_max = k;
+               max_probT = current_probT;
+           }
+
+           //////////////////////////////
+           // 2. Bayesian Updating 
+           //////////////////////////////
+
+           s[index] = sigma0 * exp(alpha * k);                                                              // computing the expression for the s_k process
+           g[index] = 1 / (sqrt2pi * s[index]) * exp(-R2[t] / (2 * s[index] * s[index])) * current_probT;   // computing the value of g at state k
+           Z += g[index];
+
+       }
+
+       // the daily volatility at k-max (this is our Volatility estimate) 
+       sigma[t] = sigma0 * exp(alpha * k_hat_max);
+       annual_vol[t] = sqrt(252) * sigma[t];       // used to create a timeseries plot of the annualized volatility 
+       printf("Annualzied volatility at time %4d is -> %3.4f\n", t, sqrt(252)*sigma[t]);
+
+       // reassigning the values of p with new posterior p-tilde
+       for (i = -t; i <= t; i += 2) {
+           ptilde[i + 1258] = g[i + 1258] / Z;     // ptilde as constructed from the conditional probability P_D_{t-1} [R_t = r_t | W_t = k] * P_D_{t-1} [W_t = k] / P_D_{t-1} [R_t = r_t] 
+           p[i + 1258] = ptilde[i + 1258];
+       }
+
    }
-
-   for (int i = 1; i <= 2 * 1260 + 1; ++i) {
-       printf("%8.17f\n", s[i]);
-   }
-
-
-   // ... and here.
-   // !!! When you have done this, get read "****" below in the Report() function.
 
    // Create TeX files for viewing results.
    Report ();
+
+   // Allows user to cancel the program 
    Exit();
 }
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // Allocate memory for an array with indices from -1260 to +1260.
@@ -79,6 +120,7 @@ double *AllocateMemory () {
 ////////////////////////////////////////////////////////////////////////////////
 // Read in a daily time series of stock price returns R[t] 1 <= t <= 1258.
 ////////////////////////////////////////////////////////////////////////////////
+
 void GetData () {
 
    int t;
@@ -111,6 +153,7 @@ void GetData () {
 ////////////////////////////////////////////////////////////////////////////////
 // Generate some output files.
 ////////////////////////////////////////////////////////////////////////////////
+
 void Report () {
 
    int t;
@@ -128,8 +171,8 @@ void Report () {
       fprintf (fp1, "%4d %10.2f\n", t, annualizedVol);
 
       // Standardize the data and add to a histogram.
-      Z = PsiInv(MTUniform(0));               // **** Get rid of this line,
-      //Z = R[t] / sigma[t];                  // and un-comment this line.
+      // Z = PsiInv(MTUniform(0));               // **** Get rid of this line,
+      Z = R[t] / sigma[t];                  // and un-comment this line.
       NormalHistogram (Z, 20, 0);
       fprintf (fp2, "%4d %10.2f\n", t, Z);
 
